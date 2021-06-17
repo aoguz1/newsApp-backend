17eea587ed102b64dbf7a101f2f323137e6dd504
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-06-17 17:12:21 +0300

    created swagger env file

--- api/src/config/settings.js
+++ api/src/config/settings.js
@@ -1,24 +1,22 @@
+import swaggerEnv from "./swaggerEnv";
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+
 module.exports = {
-  title: 'News API',
+  title: "News API",
 
   swaggerOptions: {
     swaggerDefinition: {
       info: {
-        description: 'News API',
-        title: 'News API',
-        version: '1.0.0',
+        description: "News API",
+        title: "News API",
+        version: "1.0.0",
       },
-      host: 'localhost:3000',
-      basePath: '',
-      produces: [
-        'application/json',
-        'application/xml',
-      ],
-      schemes: ['http', 'https']
+      host: swaggerEnv[env]["host"],
+      basePath: "",
+      produces: ["application/json", "application/xml"],
+      schemes: swaggerEnv[env]["address"],
     },
     basedir: __dirname, // app absolute path
-    files: [
-      '../../public/controller/**/*.js',
-    ],
-  }
+    files: ["../../public/controller/**/*.js"],
+  },
 };
---
+++ api/src/config/swaggerEnv.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    host: "localhost:3000/",
+    address: "http",
+  },
+  production: {
+    host: "news-app-mobiler.herokuapp.com",
+    address: "https",
+  },
+};
