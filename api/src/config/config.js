a9b4aa2c9a86cd4d2f5d50b3876d76dfa56d40a1
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-06-17 16:57:00 +0300

    edit config fiel

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,35 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-	development: {
-		database: 'news-backend',
-		username: 'postgres',
-		password: 'admin',
-		host: '127.0.0.1',
-		dialect: 'postgres'
-	},
-
-	docker: {
-    database: 'dockerDB',
-    username: 'postgres',
-    password: null,
-    host: 'postgres',
-    dialect: 'postgres'
+  development: {
+    database: "news-backend",
+    username: "postgres",
+    password: "admin",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
 
-	test: {
-		database: 'news-backend',
-		username: 'postgres',
-		password: null,
-		host: '127.0.0.1',
-		dialect: 'postgres'
-	},
+  docker: {
+    database: "dockerDB",
+    username: "postgres",
+    password: null,
+    host: "postgres",
+    dialect: "postgres",
+  },
 
-	production: {
-		database: 'news-backend',
-		username: 'postgres',
-		password: null,
-		host: '127.0.0.1',
-		dialect: 'postgres'
-	}
+  test: {
+    database: "news-backend",
+    username: "postgres",
+    password: null,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+
+  production: {
+    database: "d2b8p3c1e8eqb",
+    username: "wlcvmyyiizjyiz",
+    password:
+      "77035062855ef723150aa39d307013d36499219d11a0ff20ac0326a42db41099",
+    host: "ec2-3-212-75-25.compute-1.amazonaws.com",
+    dialect: "postgres",
+  },
 };
